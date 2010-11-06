0df6e6251bcede91b55eb5c6eca348a48a4b00a0
ixtix <ixtix@fixit.(none)>
2010-11-06 15:37:41 +0100

    removed netmon links

--- intrasol/conf.py
+++ intrasol/conf.py
@@ -12,15 +12,15 @@ class SettingsDefaults(object):
         "srv": u"/srv",
         "www": u"/var/www"
     }
-    EXTRACTION_MAX_FILESIZE = 1000000000
+    EXTRACTION_MAX_FILESIZE = 1000000000 # primitiv 1GB
     # Action settings
     WATCH_DEAMONIZE = True
     WATCH_PID = "/var/run/intrasol.pid"
     INDEX_CACHE_FILE = "/var/cache/intrasol.db"
     # solr settings
-    SOLR_URL = "http://solr.netmon.ch:8080"
-    SOLR_SCHEMA_PATH = "http://solr.netmon.ch:8080/solr/admin/file/?file=schema.xml"
-    SOLR_CELL_URL = "http://solr.netmon.ch:8080/solr/update/extract"
+    SOLR_URL = "http://solr.example.com:8080"
+    SOLR_SCHEMA_PATH = "http://solr.example.com:8080/solr/admin/file/?file=schema.xml"
+    SOLR_CELL_URL = "http://solr.example.com:8080/solr/update/extract"
 
 
 class Settings(object):
